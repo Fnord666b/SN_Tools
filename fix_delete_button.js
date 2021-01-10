f0571409a4533815aff7763cb43cefe0809c865f
Fnord666 <Fnord666@soylentnews.org>
2021-01-09 23:41:41 -0500

    Added fix delete button tampermonkey script

---
+++ fix_delete_button.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Fix Delete Button
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @grant        none
+// @match http://soylentnews.org/*admin.pl*
+// @match http://*.soylentnews.org/*admin.pl*
+// @match https://soylentnews.org/*admin.pl*
+// @match https://*.soylentnews.org/*admin.pl*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var db = document.getElementsByName('op');
+    if (db) {
+        for (let v of db) {
+            if (v.value == "delete") {
+                v.onclick = function() {
+                    if (confirm("Are you sure you want to delete this story?")) {
+                        st_submit(this);
+                    }else {
+                        alert("Story delete cancelled");
+                    }
+                }
+            }
+
+        }
+
+    };
+
+
+})();
